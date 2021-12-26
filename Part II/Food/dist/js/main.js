c495aa785155a2c55cd30b0aedeb4420b95bf787
Dmitriy <dm.min.zp@gmail.com>
2021-12-26 22:30:50 +0200

    work with POST-requests

--- Part II/Food/dist/js/main.js
+++ Part II/Food/dist/js/main.js
@@ -214,5 +214,49 @@ window.addEventListener('DOMContentLoaded', () => {
         'menu__item'
         ).render();
 
-    
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с Вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            
+            // request.setRequestHeader('Content-type', 'multipart/form-data');
+            const formData = new FormData(form); 
+
+            request.send(formData);
+            // отслежитвание загрузки запроса
+            request.addEventListener('load', () => {
+                // проверка статуса на OK
+                if (request.status === 200) {
+                    console.log(request.response);
+                    // если после запроса все успешно
+            statusMessage.textContent = message.success;
+
+                } else {
+            statusMessage.textContent = message.failure;
+
+                }
+            })
+        })
+    }
 });
